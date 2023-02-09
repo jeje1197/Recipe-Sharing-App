adc0b66802540ff1fb72a83c85b59da1a1344ba0
Joseph E <1jojorun@gmail.com>
2023-02-09 13:48:57 -0800

    Setup file structure for front-end

--- recipe-front-end/src/App.js
+++ recipe-front-end/src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Login from './components/Login';
+import Home from './components/Home';
+import AddRecipes from './components/AddRecipes';
+import MyRecipes from './components/MyRecipes';
 
 function App() {
-
+  let loggedIn = true
   return (
     <div className="App">
       <Header/>
 
       <Routes>
-        <Route path="/" element={ <Home/> } exact />
-        {/* <Route path="/view" element={ <ProductView setProductToUpdate = {setProductToUpdate}/>} />
-        <Route path="/add" element={<ProductCreate/>} />
-        <Route path="/update" element={<ProductUpdate productToUpdate={productToUpdate}/>} /> */}
+        <Route path="/" element={ loggedIn ? <Home/> : <Login/> } exact />
+        <Route path="/add" element={ <AddRecipes/>} />
+        <Route path="/myrecipes" element={<MyRecipes/>} />
       </Routes>
     </div>
   );
