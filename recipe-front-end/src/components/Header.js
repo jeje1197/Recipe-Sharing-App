7ab938172689ae31c242d9524b9f3a835c80892b
Joseph E <1jojorun@gmail.com>
2023-02-09 13:39:02 -0800

    .

---
+++ recipe-front-end/src/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className="display-2">Recipe Sharing App</h1>
+
+            <nav className="navbar navbar-expand-lg bg-light">
+                <div className="container-fluid">
+
+                    <Link className="navbar-brand" to="/" >Home</Link>
+
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <Link className="nav-link" to="/view">View Product</Link>
+
+                        <Link className="nav-link" to="/add">Add Product</Link>
+                    </div>
+                    </div>
+                </div>
+            </nav>
+
+        </div>
+    );
+};
+
+export default Header;
