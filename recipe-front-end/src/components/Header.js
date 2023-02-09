ce712d6333becf832b2f082007affbef79caf6c7
Joseph E <1jojorun@gmail.com>
2023-02-09 14:35:46 -0800

    Working Front-End with Page Components

--- recipe-front-end/src/components/Header.js
+++ recipe-front-end/src/components/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
             <nav className="navbar navbar-expand-lg bg-light">
                 <div className="container-fluid">
 
-                    <Link className="navbar-brand" to="/" >Home</Link>
+                    <Link className="navbar-brand" to="/">Home</Link>
 
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
---
+++ recipe-front-end/src/components/Home.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <div>Home Page Component</div>;
+}
+
+export default Home;
