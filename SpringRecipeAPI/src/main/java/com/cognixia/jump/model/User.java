7828856a1ce766b7f9a5d3829c729618d0a80427
Talha Qureshi <bmw215t@gmail.com>
2023-02-10 11:50:27 -0500

    Added more methods to user

--- SpringRecipeAPI/src/main/java/com/cognixia/jump/model/User.java
+++ SpringRecipeAPI/src/main/java/com/cognixia/jump/model/User.java
@@ -31,4 +31,46 @@ public class User implements Serializable {
 	public User() {
 		
 	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", email=" + email + "]";
+	}
+	
 }
