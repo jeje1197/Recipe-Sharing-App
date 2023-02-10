f17c92e6a08a77cc63bd5c782a53441223a00138
Talha Qureshi <bmw215t@gmail.com>
2023-02-10 16:58:12 -0500

    added delete method

--- SpringRecipeAPI/src/main/java/com/cognixia/jump/controller/UserRecipeController.java
+++ SpringRecipeAPI/src/main/java/com/cognixia/jump/controller/UserRecipeController.java
@@ -1,9 +1,11 @@
 package com.cognixia.jump.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cognixia.jump.exception.ResourceNotFoundException;
 import com.cognixia.jump.model.Recipe;
 import com.cognixia.jump.model.User;
 import com.cognixia.jump.model.UserRecipe;
@@ -45,4 +48,19 @@ public class UserRecipeController {
     public List<User> getAllUsers(@PathVariable int recipeId) {
     	return repo.getUsersFromRecipeId(recipeId + "");
     }
+    
+    @DeleteMapping("/userrecipe/{id}")
+	public ResponseEntity<?> deleteUser(@PathVariable int id) throws ResourceNotFoundException {
+		
+		Optional<UserRecipe> found = repo.findById(id);
+		
+		if( found.isEmpty() ) {
+			
+			throw new ResourceNotFoundException("UserRecipe with id = " + id + " was not found");
+		}
+		
+		repo.deleteById(id);
+		
+		return ResponseEntity.status(200).body(found.get());
+	}
 }
