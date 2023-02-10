96e1b605d68307a7d56a3526d7540a565278a5d4
Joseph E <1jojorun@gmail.com>
2023-02-10 12:25:44 -0800

    Updated Card Image Styling

--- recipe-front-end/src/components/Card.js
+++ recipe-front-end/src/components/Card.js
@@ -1,14 +1,18 @@
-
+// center image
 function Card(props) {
     return (
         <div className="card" style={{width: "18rem"}}>
-            <img src={props.image} alt="Recipe Visual"/>
+
+            <div className="cardImgDiv">
+                <img className="cardImg"src={props.image} alt="Recipe Visual"/>
+            </div>
+            
             <div className="card-body">
                 <h5 className="card-title">Meal goes here...</h5>
                 <h6 className="card-subtitle mb-2 text-muted">Author goes here...</h6>
                 <p className="card-text">optional caption goes here...</p>
-                <a href="#" className="btn btn-primary">Save</a>
-                <a href="#" className="btn btn-primary">Make</a>
+                <a href="/" className="btn btn-primary">Save</a>
+                <a href="/" className="btn btn-primary">Make</a>
             </div>
         </div>
     )
