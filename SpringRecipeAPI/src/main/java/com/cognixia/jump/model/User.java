0b4ba7298af6dab60b64e028003030c5295c5c00
Talha Qureshi <bmw215t@gmail.com>
2023-02-10 11:46:31 -0500

    Added basic User model

--- SpringRecipeAPI/src/main/java/com/cognixia/jump/model/User.java
+++ SpringRecipeAPI/src/main/java/com/cognixia/jump/model/User.java
@@ -1,5 +1,34 @@
 package com.cognixia.jump.model;
 
-public class User {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(unique = true, nullable = false)
+	@NotBlank
+	private String username;
+
+	@Column(nullable = false)
+	@NotBlank
+	private String password;
+	
+	@Column(nullable = false)
+	private String email;
+	
+	public User() {
+		
+	}
 }
